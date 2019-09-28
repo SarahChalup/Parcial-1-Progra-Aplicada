7fd92eed28cebfba7cc99556c548a25bf29453d5
sarit <sarit@DESKTOP-KSJF0BF>
2019-09-28 12:07:17 -0400

    Modelo

--- Chalup/Sarah/Models/Clown.cs
+++ Chalup/Sarah/Models/Clown.cs
@@ -1,11 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Sarah.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Tricks
+    {
+        Sopa,
+        Srampeon,
+        Superman
+    }
     public class Clown
     {
+        [Key]
+        public int ClownID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24, MinimumLength = 2)]
+
+        public string NickName { get; set; }
+        [Required]
+        public Tricks tricks { get; set; }
+        [DataType(DataType.EmailAddress, ErrorMessage = "Direccion de correo no valida")]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+
     }
 }
