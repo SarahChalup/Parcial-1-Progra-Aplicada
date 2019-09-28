7b67ee169bcca54381529a6d885695ebf718de40
sarit <sarit@DESKTOP-KSJF0BF>
2019-09-28 12:22:23 -0400

    Controlador

---
+++ Chalup/Sarah/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Sarah.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("DefaultConnection") 
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Sarah.Models.Clown> Clowns { get; set; }
+    }
+}
